07313befbbc6b105e0d361dcba545ce0189d05b0
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-03-13 04:06:53 -0700

    add skill section

--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { skills } from '../../Data/Constant'
+import { Container, Wrapper, Title, Desc, SkillsContainer, Skills, SkillTitle, SkillList, SkillItem, SkillImage } from './SkillStyle'
 
 const Skill = () => {
   return (
-    <div>
-      <h1>skill</h1>
-    </div>
+    <Container id="skill">
+      <Wrapper>
+        <Title>Skills</Title>
+        <Desc>Here are some of my skills on which I have been working.
+        </Desc>
+        <SkillsContainer>
+          {skills.map((item) => (
+            <Skills>
+              <SkillTitle>{item.title}</SkillTitle>
+              <SkillList>
+                {item.skills.map((skill) => (
+                  <SkillItem>
+                    <SkillImage src={skill.image}/>
+                    {skill.name}
+                  </SkillItem>
+                ))}
+              </SkillList> 
+            </Skills>
+          ))}
+
+        </SkillsContainer> 
+      </Wrapper>
+    </Container>
   )
 }
 

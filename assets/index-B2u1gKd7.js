292b2f27a93688d0859f69c453fc0d20a3e8f05f
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-03-20 01:44:12 -0700

    Updates

--- assets/index-B2u1gKd7.js
+++ assets/index-B2u1gKd7.js
@@ -978,7 +978,7 @@ export default theme;`}function mf(a={},...i){const{breakpoints:u,mixins:o={},sp
   background-color: ${({theme:a})=>a.bg};
   width : 100%;
   height: 100%;
-  overflow-x: hidden;
+  overflow: hidden;
   `,zm=W.div`
   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
